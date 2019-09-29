79d5e5e40b30080b313b766e4b4b162faafcae25
Tobias Schindler <tobias_schindler@hotmail.de>
2019-09-29 14:15:29 +0200

    finalized tpl

---
+++ src/shader/default_fragment_shader.glsl
@@ -0,0 +1,18 @@
+# version 300 es
+
+precision highp float;
+uniform float iTime;
+uniform vec2 iResolution;
+out vec4 fragColor;
+
+void main( )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = gl_FragCoord.xy/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
